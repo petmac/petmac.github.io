1763c10c084ffa9b67bcf53e7d12131f7b1f71bf
Peter Mackay <peter.mackay@fanduel.com>
2019-03-01 13:38:06 +0000

    Get DOM viewport

--- src/Loading.elm
+++ src/Loading.elm
@@ -1,37 +1,57 @@
 module Loading exposing (Model, Msg, init, subscriptions, update, view)
 
 import Browser exposing (Document)
+import Browser.Dom exposing (Viewport)
 import Browser.Events exposing (onAnimationFrameDelta)
 import Html exposing (Html, div, text)
+import Task
 
 
 type alias Model =
-    Float
+    Maybe Viewport
 
 
 type Msg
     = Tick Float
+    | Viewport Viewport
 
 
 init : () -> ( Model, Cmd Msg )
 init flags =
-    ( 0, Cmd.none )
+    ( Nothing
+    , Task.perform Viewport Browser.Dom.getViewport
+    )
 
 
 view : Model -> Document Msg
 view model =
     { title = "Loading"
     , body =
-        [ text (String.fromFloat model)
+        [ text (viewportText model)
         ]
     }
+
+
+viewportText : Maybe Viewport -> String
+viewportText mv =
+    case mv of
+        Just viewport ->
+            String.fromFloat viewport.viewport.width ++ " x " ++ String.fromFloat viewport.viewport.height
+
+        Nothing ->
+            "Nothing"
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Tick ms ->
-            ( ms, Cmd.none )
+            ( model, Cmd.none )
+
+        Viewport viewport ->
+            ( Just viewport
+            , Task.perform Viewport Browser.Dom.getViewport
+            )
 
 
 subscriptions : Model -> Sub Msg
