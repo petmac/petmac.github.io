f1395286bf0c2eae542e3a55707f996c2039f6aa
Peter Mackay <mackay.pete+git@gmail.com>
2019-03-01 22:54:46 +0000

    Generate a single bar

--- src/Loading.elm
+++ src/Loading.elm
@@ -1,55 +1,101 @@
 module Loading exposing (Model, Msg, init, subscriptions, update, view)
 
 import Browser exposing (Document)
-import Browser.Dom exposing (Viewport)
 import Browser.Events exposing (onAnimationFrameDelta)
+import Css exposing (..)
 import Html.Styled exposing (Html, div, text, toUnstyled)
+import Html.Styled.Attributes exposing (css)
+import Random
 import Task
 
 
+type alias Bar =
+    { top : Float
+    , height : Float
+    , color : Color
+    }
+
+
 type alias Model =
-    Maybe Viewport
+    { bars : Maybe (List Bar)
+    }
 
 
 type Msg
     = Tick Float
-    | Viewport Viewport
+    | Bars (List Bar)
 
 
 init : () -> ( Model, Cmd Msg )
 init flags =
-    ( Nothing
-    , Task.perform Viewport Browser.Dom.getViewport
+    ( { bars = Nothing }
+    , Cmd.batch [ generateBars ]
     )
+
+
+generateBars : Cmd Msg
+generateBars =
+    Random.generate Bars barsGenerator
+
+
+barsGenerator : Random.Generator (List Bar)
+barsGenerator =
+    Random.list 1 barGenerator
+
+
+barGenerator : Random.Generator Bar
+barGenerator =
+    Random.map2
+        (\h col -> { top = 0, height = h, color = col })
+        (Random.float 5 10)
+        colorGenerator
+
+
+colorGenerator : Random.Generator Color
+colorGenerator =
+    Random.uniform (hex "ff0000") [ hex "00ff00", hex "0000ff" ]
 
 
 view : Model -> Document Msg
 view model =
     { title = "Loading"
     , body =
-        case model of
-            Just viewport ->
-                [ String.fromFloat viewport.viewport.width
-                    ++ " x "
-                    ++ String.fromFloat viewport.viewport.height
-                    |> text
-                    |> toUnstyled
-                ]
+        case model.bars of
+            Just bars ->
+                List.map viewBar bars
+                    |> List.map toUnstyled
 
             Nothing ->
                 []
     }
 
 
+viewBar : Bar -> Html Msg
+viewBar bar =
+    div
+        [ css
+            [ position absolute
+            , left (pct 0)
+            , top (pct bar.top)
+            , width (pct 100)
+            , height (pct bar.height)
+            , backgroundColor bar.color
+            ]
+        ]
+        []
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Tick ms ->
-            ( model, Cmd.none )
+            ( model
+            , Cmd.batch [ generateBars ]
+            )
 
-        Viewport viewport ->
-            ( Just viewport
-            , Task.perform Viewport Browser.Dom.getViewport
+        Bars bars ->
+            ( { model | bars = Just bars }
+            , Cmd.none
             )
 
 
